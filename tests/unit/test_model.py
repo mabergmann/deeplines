cf3dc91b3a53a9d31765aeec7bd0261be8ebdfaf
Matheus Bergmann <matheus.bergmann@pixforce.com.br>
2023-02-07 22:46:39 -0300

    :sparkles: Create simple model

---
+++ tests/unit/test_model.py
@@ -0,0 +1,21 @@
+import pytest
+import torch
+from torch import nn
+
+from deeplines.model import DeepLines
+
+
+@pytest.fixture
+def model():
+    return DeepLines((800, 800), 9)
+
+
+def test_model_is_nn_module(model):
+    assert isinstance(model, nn.Module)
+
+
+def test_expected_output_shape(model):
+    input_batch = torch.zeros((1, 3, 800, 800))
+    with torch.no_grad():
+        output = model(input_batch)
+    assert output.shape == torch.Size([1, 9, 1])
